6d8cb4396504cc3ec0c4f84a6a7fe339181a5f5f
Louis <louis.anth10@gmail.com>
2025-12-22 18:22:16 +0700

    homepage done(tailwind fix)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Tambahkan ini
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Tambahkan ini
+  ],
 })
